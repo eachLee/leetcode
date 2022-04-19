750f5b6067968b075f0d745039a163a1d49e9428
likl7 <likl7@centaline.com.cn>
2022-04-19 14:02:32 +0800

    14.最长公共前缀

--- 14.最长公共前缀.js
+++ 14.最长公共前缀.js
@@ -6,35 +6,29 @@
 var strs = ["reflower", "flow", "flight"];
 
 var longestCommonPrefix = function (strs) {
+	//只有一个值的时候 直接返回
+	if (strs.length === 1) {
+		return strs[0];
+	}
+	//进行长短排序 减少循环次数
 	strs = strs.sort((a, b) => a.length - b.length)
 	let sortStr = strs[0]
 	let strsLen = sortStr.length;
+	let prefixStr = '',
+		flag;
 	if (!strsLen) return '';
-	for (let i = 0; i <= strsLen; i++) {
-		let prefixStr = sortStr.slice(0, i);
-		if (i === 0) {
-			prefixStr = sortStr.charAt(0)
-		}
-		//判断只有一个的时候 直接返回
-		if (strs.length === 1) {
-			return strs[0];
-		}
-		let flag = strs.every((item) => {
-			if (item.startsWith(prefixStr)) {
+	while (!flag) {
+		prefixStr = sortStr.slice(0, prefixStr.length + 1);
+		flag = strs.find((item) => {
+			if (!item.startsWith(prefixStr)) {
 				return true;
 			} else {
 				return false;
 			}
 		})
-		if (!flag) {
-			if (i === 0) {
-				return "";
-			}
-			return prefixStr.slice(0, -1);
-		} else {
-			if (i === strsLen) {
-				return prefixStr;
-			}
+		if (!flag && prefixStr === sortStr) {
+			return prefixStr;
 		}
 	}
+	return prefixStr.slice(0, -1);
 };
